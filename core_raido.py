d0162dd3ea01e43651e54c292aae9db11c338985
ali-ghazi78 <alighaziasgar75@gmail.com>
2021-02-01 01:38:49 +0330

    core_raidio gui is okay and 3 tabs out of 4 works

--- core_raido.py
+++ core_raido.py
@@ -9,22 +9,37 @@ from PyQt5.QtGui import QPixmap
 from save_image import *
 import datetime
 from  edit_book_radio import * 
-
-ui_path = os.path.join(os.getcwd(), "gui/booking.ui")
+import icon_rc
+from radio_search import * 
+from radio_add import * 
+from booking import * 
+ui_path = os.path.join(os.getcwd(), "gui/core_radio.ui")
 Form = uic.loadUiType(ui_path)[0]
 
 
 
-class Booking(QMainWindow, Form):
+class Core_page(QMainWindow, Form):
     def __init__(self):
         QMainWindow.__init__(self)
         Form.__init__(self)
         self.setupUi(self)
 
+        self.add_window = RadioADD()
+        self.vertic_1.addWidget(self.add_window)
+
+        self.search_window = RadioSearch()
+        self.vertic_2.addWidget(self.search_window)
+
+        self.all_booking = Booking()
+        self.vertic_4.addWidget(self.all_booking)
+
+        
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    q = Booking()
+    q = Core_page()
     q.show()
     sys.exit(app.exec_())
 
