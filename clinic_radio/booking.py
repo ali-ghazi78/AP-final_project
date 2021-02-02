13923ea22cfb039b4ceb4a4e8b857e58b7b7c15a
ali-ghazi78 <alighaziasgar75@gmail.com>
2021-02-02 16:52:15 +0330

    booking is okey but selected_date visits is not shown

--- clinic_radio/booking.py
+++ clinic_radio/booking.py
@@ -6,7 +6,7 @@ from sql_connector2 import *
 import sys
 import os
 from PyQt5.QtGui import QPixmap
-# from save_image import *
+from choose_patient import *
 import datetime
 from booking_rc import *
 
@@ -15,14 +15,52 @@ ui_path = os.path.join(os.path.dirname(os.getcwd()),
 Form = uic.loadUiType(ui_path)[0]
 
 
-
 class BookAP(QMainWindow, Form):
     def __init__(self):
         QMainWindow.__init__(self)
         Form.__init__(self)
         self.setupUi(self)
+        self.select_patient.clicked.connect(self._select_patient)
+        self.select_doctor.clicked.connect(self._select_doctor)
+        self.book_ap.clicked.connect(self._book_ap)
+
+        self.patient_pass_id = ""
+        self.doctor_pass_id = ""
+
+    def _book_ap(self):
+        if(self.patient_pass_id != "" and self.doctor_pass_id !=""):
+            time = self.visit_time.time().toString("hh:mm:ss")
+            date = self.visit_date.selectedDate().toString("yyyy-MM-dd")
+            datetime = date + " " + time
+            k = {
+
+                "patient_pass_id": self.patient_pass_id,
+                "doctor_pass_id": self.doctor_pass_id,
+                "visit_date" : datetime
+            }
+            insert_into_table("clinic", "booking", k)
+            QMessageBox.warning(
+                self, " ", "رزور با موفقیت انجام شد ")
+        else:
+          QMessageBox.warning(
+                self, " ", "دکتر یا بیمار به درستی انتخاب نشده است ")
 
 
+    def _select_patient(self):
+        self.choose_patient_window = ChoosePerson(self, "patient")
+        self.choose_patient_window.show()
+
+    def _select_doctor(self):
+        self.choose_doctor_window = ChoosePerson(self, "doctor")
+        self.choose_doctor_window.show()
+
+    def update(self, kargs):
+        if("patient_last_name" in kargs):
+            self.patient_last_name.setText(kargs["patient_last_name"])
+            self.patient_pass_id = (kargs["patient_pass_id"])
+        elif("doctor_last_name" in kargs):
+            self.doctor_last_name.setText(kargs["doctor_last_name"])
+            self.doctor_pass_id = (kargs["doctor_pass_id"])
 
 
 if __name__ == "__main__":
@@ -30,4 +68,3 @@ if __name__ == "__main__":
     q = BookAP()
     q.show()
     sys.exit(app.exec_())
-
--- clinic_radio/choose_patient.py
+++ clinic_radio/choose_patient.py
@@ -14,28 +14,43 @@ ui_path = os.path.join(os.path.dirname(os.getcwd()),
 Form = uic.loadUiType(ui_path)[0]
 
 
-class ChoosePatient(QMainWindow, Form):
-    def __init__(self):
+class ChoosePerson(QMainWindow, Form):
+    def __init__(self,partner_window=None,patient_or_doctor=None):
         QMainWindow.__init__(self)
         Form.__init__(self)
         self.setupUi(self)
-
+        self.partner_window = partner_window
         self.all_fields = [self.first_name,
                            self.last_name, self.father_name, self.pass_id]
+
+        self.all_fields_selected = [self.first_name_s,
+                           self.last_name_s, self.father_name_s, self.pass_id_s]
         self.show_all.clicked.connect(self._show_all)
         self.search.clicked.connect(self._search)
         self.clearField.clicked.connect(self._clearField)
-        # self.tableWidget.cellClicked.connect(self._show_cell_image)
-
-
+        self.tableWidget.cellClicked.connect(self._choose_person)
+        self.select_person.clicked.connect(self._select_person)
         self.tableWidget.verticalHeader().setDefaultSectionSize(100)
         self.tableWidget.horizontalHeader().setDefaultSectionSize(100)
+        self.patient_or_doctor = patient_or_doctor
+
+    def _select_person(self):
+        
+        if(self.partner_window!=None):
+            f = {
+               (self.patient_or_doctor+"_last_name"):self.last_name_s.text(),
+                (self.patient_or_doctor+"_pass_id"):self.pass_id_s.text(),
+            }
+            self.partner_window.update(f)
+        self.close()
 
 
     def _clearField(self):
         for i in self.all_fields:
             i.setText("")
-
+        for i in self.all_fields_selected:
+            i.setText("")
+            
     def _search(self):
         my_dict = {}
         for i in self.all_fields:
@@ -43,22 +58,28 @@ class ChoosePatient(QMainWindow, Form):
                 obj_name = i.objectName()
                 my_dict[obj_name] = (i.text())
         col = ["first_name", "last_name","father_name","pass_id", "image"]
-        re = search_for_record("clinic","patient_info",my_dict,col)
+       
+        if(self.patient_or_doctor=="doctor"):
+            re = search_for_record("clinic","doctor_info",my_dict,col)
+        else:
+            re = search_for_record("clinic","patient_info",my_dict,col)
+
         self.show_on_table(re,[4,])
         
     def _show_all(self):
+
         col = ["first_name", "last_name","father_name","pass_id", "image"]
-        re = search_for_record("clinic","patient_info",{"first_name":".*"},col)
+        if(self.patient_or_doctor=="doctor"):
+            re = search_for_record("clinic","doctor_info",{"first_name":".*"},col)
+        else:
+            re = search_for_record("clinic","patient_info",{"first_name":".*"},col)
         self.show_on_table(re,[4,])
-
-
 
 
     def show_on_table(self, re,image_col):
         if(len(re) != 0):
             self.tableWidget.setRowCount(len(re))
             self.tableWidget.setColumnCount(len(re[0]))
-            # self.tableWidget.setItem(1, 1, QTableWidgetItem("TEXT"))
             for i in range(len(re)):
                 for j in range(len(re[i])):
                     if j not in image_col:
@@ -67,12 +88,11 @@ class ChoosePatient(QMainWindow, Form):
                     else:
                         if re[i][j] != None:
                             self.tableWidget.setCellWidget(
-                            i, j, (ImgWidget1(re[i][j])))
-            # for i in range(len(re)):
-            #     lb = QLabel(self)
-            #     lb.setText('نمایش تصویر')
-            #     self.tableWidget.setCellWidget(i, len(re[0]), lb)
-        
+                            i, j, (ImgWidget1(re[i][j],0)))
+                        else:
+                            self.tableWidget.setCellWidget(
+                            i, j, (ImgWidget1(re[i][j],1)))
+
         else:
             self.tableWidget.setRowCount(0)
             g = self.tableWidget.columnCount()
@@ -81,25 +101,26 @@ class ChoosePatient(QMainWindow, Form):
             QMessageBox.warning(
                 self, " ", "چیزی پیدا نشد ")
 
-    # def _show_cell_image(self,row, col):
-    #     if(col==5):# if show image col was called
-    #         pa_id = self.tableWidget.item(row, 3).text() #pass_id
-    #         vi_date = self.tableWidget.item(row, 4).text() #visit_date
-    #         loadImage(pa_id,vi_date)
-
+    def _choose_person(self,row, col):
+        for i in range(len(self.all_fields_selected)):
+            self.all_fields_selected[i].setText( self.tableWidget.item(row, i).text())
+        
 
 class ImgWidget1(QLabel):
-    def __init__(self,data, parent=None):
+    def __init__(self,data, reset,parent=None):
         super(ImgWidget1, self).__init__(parent)
-        mp = QPixmap()
-        mp.loadFromData(data)
-        mp = mp.scaled(100,100)
-        self.setPixmap(mp)
+        if(reset):
+            self.clear()
+        else:
+            mp = QPixmap()
+            mp.loadFromData(data)
+            mp = mp.scaled(100,100)
+            self.setPixmap(mp)
 
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    q = ChoosePatient()
+    q = ChoosePerson()
     q.show()
     sys.exit(app.exec_())
